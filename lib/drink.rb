f3c91a75841c45b12ca9130e7246dcd108087686
prodigysurge <boatinglife6@yahoo.com>
2021-02-03 19:38:14 -0500

    sets class Drink

--- lib/drink.rb
+++ lib/drink.rb
@@ -1,3 +1,18 @@
 class Drink 
 
+    attr_accessor :name, :id, :liquor
+
+    @@all_drinks = []
+
+    def initialize(hash)
+        hash.each do |k,v|
+            self.send("#{k}=", "#{v}") if self. respond_to?("#{k}=")
+        end
+
+        @@all_drinks << self
+    end
+
+    def self.all_drinks
+        @@all_drinks
+    end
 end
