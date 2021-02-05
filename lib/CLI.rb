7d09a2e0a99bf6db140af140c45f0e9cef334371
prodigysurge <boatinglife6@yahoo.com>
2021-02-05 16:15:16 -0500

    sets up the initial terminal table for listing of all drinks within the liquor/spirits CLI.user_input_liquor

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -38,18 +38,18 @@ class CLI
 
     def top_ten_drinks
         rows = []
-        rows << ['1.', 'Mojito']
-        rows << ['2.', 'Old Fashioned']
-        rows << ['3.', 'Long Island Tea']
-        rows << ['4.', 'Negroni']
-        rows << ['5.', 'Mai Tai']
-        rows << ['6.', 'Dry Martini']
-        rows << ['7.', 'Daiquiri']
-        rows << ['8.', 'Margarita']
-        rows << ['9.', 'Manhattan']
-        rows << ['10.', 'Moscow Mule']
+        rows << ['1', 'Mojito']
+        rows << ['2', 'Old Fashioned']
+        rows << ['3', 'Long Island Tea']
+        rows << ['4', 'Negroni']
+        rows << ['5', 'Mai Tai']
+        rows << ['6', 'Dry Martini']
+        rows << ['7', 'Daiquiri']
+        rows << ['8', 'Margarita']
+        rows << ['9', 'Manhattan']
+        rows << ['10', 'Moscow Mule']
         rows << ['11'.colorize(:red), 'Search by liquor'.colorize(:red)]
-        table = Terminal::Table.new :title => "Top Ten Drinks World Wide".colorize(:green), :headings => ['Option'.colorize(:yellow), 'Drinks'.colorize(:yellow)], :rows => rows
+        table = Terminal::Table.new :title => "Top Ten Drinks World Wide".colorize(:green), :headings => ['Enter'.colorize(:yellow), 'For Drink'.colorize(:yellow)], :rows => rows
         puts table
         input = gets.strip
         self.user_input_drink(input) 
@@ -58,10 +58,12 @@ class CLI
     def user_input_drink(input)
         top_ten = {"1" => "Mojito", "2" => "Old Fashioned", "3" => "Long Island Tea", "4" => "Negroni", "5" => "Mai Tai", "6" => "Dry Martini", "7" => "Daiquiri", "8" => "Margarita", "9" => "Manhattan", "10" => "Moscow Mule"}
         input_value = top_ten[(input)]
-        if input.to_i.between?(1,11) 
+        if input.to_i.between?(1,10) 
             new_recipe = Api.get_drink_by_name(input_value)
         elsif input == "q" || input == "Q" || input == "End" || input == "end"
             exit_program
+        elsif input == "11" || input == "liquor" || input == "Liquor" || input == "start ovr"
+            request_from_liquor
         else
             puts "Your input was invalid.".colorize(:red)
             self.ask_for_drink_or_input
@@ -70,20 +72,24 @@ class CLI
     end
 
     def recipe_display(new_recipe)
-        puts "All the wonderful information you requested for your #{new_recipe.name} drink.".colorize(:green)
+        puts "All the wonderful information you requested for your".colorize(:green) + " #{new_recipe.name.colorize(:yellow)}" + " drink.".colorize(:green)
+        amounts_of_ingredients = new_recipe.ingredients.zip(new_recipe.amounts)
         puts "The recommended glass to use for your".colorize(:green) +  " #{new_recipe.name.colorize(:yellow)}" + ", is the".colorize(:green) + " #{new_recipe.glass.colorize(:yellow)}" + ".".colorize(:green)
         puts "The ingredients and their amounts to use are:".colorize(:green)
         puts ""
         amounts_of_ingredients = new_recipe.ingredients.zip(new_recipe.amounts)
         amounts_of_ingredients.each do |ing, amt|
-            puts "#{ing}: #{amt}"
+            puts "#{ing}:".colorize(:yellow) + " #{amt}" 
         end
         puts ""
         puts "Now the best part! Time to make your drink!".colorize(:green)
         puts ""
         puts "#{new_recipe.instructions}".colorize(:yellow)
-        main_menu
+        self.main_menu
+
     end
+
+
 
     
 ########################################
@@ -106,11 +112,13 @@ class CLI
     def user_input_liquor(input)
        
         liquor_list = {"1" => "Brandy", "2" => "Gin", "3" => "Rum", "4" => "Tequila", "5" => "Vodka", "6" => "Whisky"}
-        input_value = top_ten[(input)]
-        if input.to_i.between?(1,7) 
+        input_value2 = liquor_list[(input)]
+        if input.to_i.between?(1,6) 
             new_drink = Api.get_drink_by_liquor(input_value2)
         elsif input == "q" || input == "Q"
-            user_quit
+            self.user_quit
+        elsif input == "7" || input == "search" || input == "start over" || input == "top ten" || input == "Top Ten" || input == "Top ten"
+            self.top_ten_drinks
         else
             puts "Your input was invalid".colorize(:red)
             self.request_from_liquor
@@ -123,7 +131,14 @@ class CLI
         puts "Thank you, here is the list of drinks from your selection."
         puts ""
         puts "Please make a selection from the following list by entering  the correlating number."
-        new_drink.each_with_index{|j,i| puts "#{i +1}. #{j.name}"}
+        new_drink.each_with_index do |j,i|             # "#{i +1}. #{j.name}"}
+            rows = []
+            rows << ["#{i +1}".colorize(:green), "#{j.name}".colorize(:light_blue)] 
+            
+            table = Terminal::Table.new :title => "Our list of drinks".colorize(:green), :headings => ['Enter'.colorize(:yellow), 'For Drink'.colorize(:yellow)], :rows => rows
+            puts table
+        
+            
         input = gets.strip
         input = input.to_i
             if (1..new_drink.count) === input
@@ -135,15 +150,9 @@ class CLI
             else 
                 puts "Your input was invalid".colorize(:red)
             end
-        self.display_of_drinks(new_drink)
+            self.display_of_drinks(new_drink)
         end
-    
-
-
-
-
-
-########################################
+    end
 
     def main_menu
         puts ""
@@ -167,3 +176,4 @@ class CLI
 
     
 end
+
